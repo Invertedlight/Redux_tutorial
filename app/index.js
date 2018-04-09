1cf74b616a757b02461946e3e2e88f5d26965e5b
James Richardson <James.Richardson@invertedlight.com>
2018-04-09 14:10:31 -0500

    Redux project initialization
    Base setup of project environment

---
+++ app/index.js
@@ -0,0 +1,15 @@
+function createStore () {
+	// The store has 4 parts
+	// 1 internal state
+	// 2 get state
+	// 3 listen to changes on state
+	// 4 update changes on state
+
+	let state
+
+	const setState = () => state 
+
+		return {
+			getState		}
+	}
+
